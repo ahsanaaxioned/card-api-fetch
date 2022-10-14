0582883e8d9e046ac3e128a1d433e90af412b60f
ahsanaaxioned <ahsana@axioned.com>
2022-10-14 17:52:43 +0530

    js changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,55 +1,28 @@
 // global variable declaration start here
 const url = "https://jsonplaceholder.typicode.com/posts";
 const fetchList = document.querySelector(".fetch-list");
-const btn = document.querySelector(".btn");
-let result;
-let limit = 10;
-let i = 0;
 // global variable declaration start here
-// fatching API start here
-fetch(url)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        setData(data);
-    });
-function setData(data) {
-    result = data;
-}
-// fatching API end here
-btn.addEventListener("click", firstShow);
-
-function firstShow() {
-    for (i; i < limit; i++) {
-        if (i == 100) {
-            btn.classList.add("btn-active");
-            let p = document.createElement("p");
-            p.innerText = "no more data left to show...Thank you..!";
-            document.body.appendChild(p)
-        }
+// element creat function start here
+function showCard(data) {
+    data.forEach(function (values) {
         let li = document.createElement("li");
         li.classList.add("fetch-item");
-        li.innerHTML = `<span class="heading">${result[i].id}</span>
-        <h3 class="title">${result[i].title}</h3>
-        <p class="discription">${result[i].body}</p>`
+        li.innerHTML = `<span class="heading">${values.id}</span>
+        <h3 class="title">${values.title}</h3>
+        <p class="discription">${values.body}</p>`
         fetchList.appendChild(li);
-    }
-    i = limit;
-    limit += limit;
+    });
+}
+// element creat function end here
+// fatching API start here
+function fetchApi(api) {
+    fetch(api)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            showCard(data);
+        });
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+fetchApi(url);
+// fatching API end here
